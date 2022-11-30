e2d82a0bf00d4e33a0c22ea54a4cb02c300ffa94
Vaibhav <vs630916@gmail.com>
2022-12-01 01:24:20 +0530

    Adding Pokecard file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
-import Pokedex from "./Pokedex";
-import "./App.css";
+import Pokecard from "./Pokecard";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Pokedex />
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="App">
+            <Pokecard
+            id={4}
+            name="Charmander"
+            type="fire"
+            exp={62}
+            />
+         </div>
+      )
+   }
 }
 
 export default App;
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
-import "./Pokecard.css";
-const POKE_API =
+import './Pokecard.css';
+
+const POKI_API =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 class Pokecard extends Component {
   render() {
-    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    let imgSrc = `${POKI_API}${this.props.id}.png`; // for adding pokemon image
     return (
       <div className="Pokecard">
         <h1>{this.props.name}</h1>
-        <img src={imgSrc} alt={this.props.name} />
-        <div>Type: {this.porps.type} </div>
-        <div>EXP: {this.porps.exp} </div>
+        <img src={imgSrc} alt={this.props.name}/>
+        <div>Type: {this.props.type}</div>
+        <div>EXp: {this.props.exp}</div>
       </div>
     );
   }
